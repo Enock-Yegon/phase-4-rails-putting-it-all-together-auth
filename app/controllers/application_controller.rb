ede78d279c6cd01a21715649e25d2b48b3c44dbe
Enock-Yegon <ykibet70@gmail.com>
2023-03-29 13:08:33 +0300

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  
+
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   before_action :authorize
+
+  
 
   private
 
@@ -16,4 +18,6 @@ class ApplicationController < ActionController::API
   def render_unprocessable_entity_response(exception)
     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
+
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,19 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorize, only: :create
-  
-    def create
+  skip_before_action :authorize, only: :create
+
+  def create
       user = User.find_by(username: params[:username])
       if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: user
+          session[:user_id] = user.id
+          render json: user
       else
-        render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+          render json: { errors: ["Invalid username or password"] }, status: :unauthorized
       end
-    end
-  
-    def destroy
+  end
+
+  def destroy
       session.delete :user_id
       head :no_content
-    end
   end
+
+end
